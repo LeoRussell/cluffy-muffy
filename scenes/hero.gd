b93ddf4b1e9c63c746a243503d14ed6974a17a01
LeoRussell <leo.russell@mail.ru>
2024-03-10 14:28:27 +0300

    added animation and some other sprites

--- scenes/hero.gd
+++ scenes/hero.gd
@@ -1,8 +1,21 @@
 extends CharacterBody2D
 
-const SPEED = 300	
+@onready var _animated_sprite = $HeroMovingSprite
+
+const SPEED = 200	
 
 func _physics_process(delta):
+	if Input.is_action_pressed("move_left"):
+		_animated_sprite.play("move")
+	elif Input.is_action_pressed("move_up"):
+		_animated_sprite.play("move")
+	elif Input.is_action_pressed("move_right"):
+		_animated_sprite.play("move")
+	elif Input.is_action_pressed("move_down"):
+		_animated_sprite.play("move")
+	else:
+		_animated_sprite.play("idle")
+		
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * SPEED
 	move_and_slide()

0a6327f940df25ebeffacce07cd5a545fcd7db3d
LeoRussell <leo.russell@mail.ru>
2024-03-10 15:41:57 +0300

    added collision object

--- scenes/hero.gd
+++ scenes/hero.gd
@@ -6,13 +6,21 @@ const SPEED = 200
 
 func _physics_process(delta):
 	if Input.is_action_pressed("move_left"):
+		# another anim in future
 		_animated_sprite.play("move")
+		
 	elif Input.is_action_pressed("move_up"):
+		# another anim in future
 		_animated_sprite.play("move")
+		
 	elif Input.is_action_pressed("move_right"):
+		# another anim in future
 		_animated_sprite.play("move")
+		
 	elif Input.is_action_pressed("move_down"):
+		# another anim in future
 		_animated_sprite.play("move")
+		
 	else:
 		_animated_sprite.play("idle")
 		
--- scenes/heromove.gd
+++
@@ -1,9 +0,0 @@
-extends AnimatedSprite2D
-
-@onready var _animated_sprite = $AnimatedSprite2D
-
-func _process(_delta):
-	if Input.is_action_pressed("move_left"):
-		_animated_sprite.play("run")
-	else:
-		_animated_sprite.stop()

47b2766dd62079f262ef948c5fbdcc543cfbe866
LeoRussell <leo.russell@mail.ru>
2024-03-16 19:00:16 +0300

    added blank enemy and blank projectile

---
+++ scenes/enemyf.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+
+@onready var _animated_sprite = $EnemyFSprite
+
+func _process(delta):
+	_animated_sprite.play("idle")
--- scenes/hero.gd
+++ scenes/hero.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@export var projectile : PackedScene
 @onready var _animated_sprite = $HeroSprite
 
 const SPEED = 175	
@@ -11,10 +12,11 @@ func _physics_process(delta):
 	# idk how to write it better. XP
 	elif Input.is_action_pressed("move_up") or Input.is_action_pressed("move_down"):
 		_animated_sprite.play("move")
-		
+				
 	else:
 		_animated_sprite.play("idle")
 		
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * SPEED
 	move_and_slide()
+

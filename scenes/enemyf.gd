7c7c79f5321d96bbf148e5a369813f3d448c215b
LeoRussell <leo.russell@mail.ru>
2024-03-16 23:33:15 +0300

    added some animation

--- scenes/enemyf.gd
+++ scenes/enemyf.gd
@@ -1,7 +1,25 @@
 extends StaticBody2D
 
-
+var health = 3
 @onready var _animated_sprite = $EnemyFSprite
 
-func _process(delta):
+
+func _on_ready():
 	_animated_sprite.play("idle")
+
+
+func _process(delta):
+	pass
+
+
+func get_damage(points):
+	health -= points
+	if health <= 0:
+		$CollisionShape2D.queue_free()
+		
+		$DeathTimer.start()
+		_animated_sprite.play("death")
+		
+		
+func _on_death_timer_timeout():
+	queue_free()
--- scenes/projectileball.gd
+++ scenes/projectileball.gd
@@ -1,26 +1,33 @@
 extends CharacterBody2D
 
 @onready var _animated_sprite = $BallSprite
-@onready var ball = $ProjectileBall
 
 @onready var direction = Input.get_vector("fire_left", "fire_right", "fire_up", "fire_down")
 const SPEED = 250
 
 
 func _on_ready():
-	$Timer.start()
+	_animated_sprite.play("flight")
 
 func _process(delta):
-	_animated_sprite.play("flight")
 	velocity = direction * SPEED
 	
 	move_and_slide()
 
-func _on_ball_signal_area_entered(area):
-	queue_free()
 
 func _on_ball_signal_body_entered(body):
-	queue_free()
+	print(body.name)
+	# damage dealing seaction
+	if body.name.begins_with("enemyF"):
+		body.get_damage(1)
+	
+	# ball fade animation
+	direction = -direction
+	$BallSignal.queue_free()
+	_animated_sprite.play("broken")
+	$Timer.start()
+	
+	
 	
 func _on_timer_timeout():
 	queue_free()

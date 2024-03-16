e3d68fb59792c70dbb7a184c8a92f2fe5690b2f7
LeoRussell <leo.russell@mail.ru>
2024-03-16 22:28:48 +0300

    finally made a projectile.

--- scenes/enemyf.gd
+++ scenes/enemyf.gd
@@ -1,17 +1,7 @@
-extends Area2D
+extends StaticBody2D
 
 
 @onready var _animated_sprite = $EnemyFSprite
 
 func _process(delta):
 	_animated_sprite.play("idle")
-
-
-func _on_body_entered(body):
-	if body.name == "ProjectileBall":
-		body.queue_free()
-
-
-func _on_body_exited(body):
-	if body.name == "ProjectileBall":
-		body.queue_free()
--- scenes/projectileball.gd
+++ scenes/projectileball.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-
 @onready var _animated_sprite = $BallSprite
+@onready var ball = $ProjectileBall
+
 @onready var direction = Input.get_vector("fire_left", "fire_right", "fire_up", "fire_down")
-const SPEED = 200
+const SPEED = 250
 
 
 func _on_ready():
@@ -15,6 +16,14 @@ func _process(delta):
 	
 	move_and_slide()
 
+func _on_ball_signal_area_entered(area):
+	queue_free()
 
+func _on_ball_signal_body_entered(body):
+	queue_free()
+	
 func _on_timer_timeout():
 	queue_free()
+
+
+
